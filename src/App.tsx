3bc17e244c68f82d4f8998acffbd9a80dc2f6d9f
Leon564 <nleon564.nl@gmail.com>
2024-06-15 22:21:46 -0600

    Add Suspense and ErrorBoundary components to App.js

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
 import { BrowserRouter } from "react-router-dom";
 import Routes from "./routes";
 import AuthProvider from "./auth.context";
+import ErrorBoundary from "antd/es/alert/ErrorBoundary";
+import { Suspense } from "react";
+import LoadingScreen from "./components/common/loadingScreen";
 
 const App = () => {
+ 
   return (
-    <BrowserRouter>
-      <AuthProvider>
-        <Routes />
-      </AuthProvider>
-    </BrowserRouter>
+    <Suspense fallback={<LoadingScreen />}>
+    <ErrorBoundary>
+      <BrowserRouter>
+        <AuthProvider>
+          <Routes />
+        </AuthProvider>
+      </BrowserRouter>
+    </ErrorBoundary>
+    </Suspense>
   );
 };
 
